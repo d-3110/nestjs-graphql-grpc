84e628b2764f35ac004fcc21f6900d1e89ad4361
daisuke saito <higashiishiyama.5983@gmail.com>
2024-01-06 12:25:15 +0900

    Implement find many for tables with relations

--- src/modules/meals/meals.service.ts
+++ src/modules/meals/meals.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMealInput } from './dto/create-meal.input';
 import { UpdateMealInput } from './dto/update-meal.input';
+import { PrismaService } from 'nestjs-prisma';
+import { PrismaPromise } from '@prisma/client';
+import { Meal } from './entities/meal.entity';
 
 @Injectable()
 export class MealsService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createMealInput: CreateMealInput) {
     return 'This action adds a new meal';
   }
 
-  findAll() {
-    return `This action returns all meals`;
+  findAll(): PrismaPromise<Meal[]> {
+    return this.prisma.meal.findMany({
+      include: {
+        _count: {
+          select: {
+            meal_tags: {},
+            histories: {},
+          },
+        },
+        meal_tags: {
+          include: {
+            tags: {},
+          },
+        },
+        histories: {},
+      },
+    });
   }
 
   findOne(id: number) {
